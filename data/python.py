85c047baf37179f9b072eec8455f846d288eac7c
Saurabh Jhanjee <jhanjeesaurabh@gmail.com>
2021-08-21 01:23:39 +1000

    Removed clutter

--- data/python.py
+++
@@ -1,9 +0,0 @@
-import os
-from matplotlib import pyplot as plt
-
-with open("data/polygon_irregular.txt") as file:
-    xs, ys = list(zip(*list(map(lambda i: list(map(float, i.split())), file.readlines()))))
-    for (i, (x, y)) in enumerate(zip(xs, ys)):
-        plt.text(x, y, str(i))
-    plt.plot(xs, ys)
-    plt.show()
